22f23dd63c2763c4361ea6171082aac05c227d2f
aaravsharma <aaravsharma927@gmail.com>
2020-09-08 00:43:32 -0700

    Given a String of array return group anagrams and its Junit Tests.

--- src/main/java/org/sharma/aarav/java/algorithms/strings/Anagram.java
+++ src/main/java/org/sharma/aarav/java/algorithms/strings/Anagram.java
@@ -2,8 +2,14 @@ package org.sharma.aarav.java.algorithms.strings;
 
 //5. Write a program to decide if two strings are anagrams or not.
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Anagram {
 
+    //Find if two strings are anagrams or not.
     public boolean isAnagram(String s1, String s2) {
 
         if(s1==null || s2==null) return false;
@@ -19,7 +25,30 @@ public class Anagram {
             if(c1[i]!=c2[i]) return false;
         }
         return true;
+    }
 
+    //Given a String of array return group anagrams.
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> result = new ArrayList<List<String>>();
+
+        Map<String, ArrayList<String>> map = new HashMap<String, ArrayList<String>>();
+        for(String str: strs){
+            char[] arr = new char[26];
+            for(int i=0; i<str.length(); i++) {
+                arr[str.charAt(i)-'a']++;
+            }
+            String ns = new String(arr);
+
+            if(map.containsKey(ns)) {
+                map.get(ns).add(str);
+            } else {
+                ArrayList<String> al = new ArrayList<String>();
+                al.add(str);
+                map.put(ns, al);
+            }
+        }
+        result.addAll(map.values());
+        return result;
     }
 
 }
--- src/test/java/org/sharma/aarav/java/algrothims/strings/AnagramTest.java
+++ src/test/java/org/sharma/aarav/java/algrothims/strings/AnagramTest.java
@@ -2,10 +2,16 @@ package org.sharma.aarav.java.algrothims.strings;
 
 //5. Junit to test if two strings are anagrams or not.
 
+import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.sharma.aarav.java.algorithms.strings.Anagram;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class AnagramTest {
 
@@ -37,8 +43,35 @@ public class AnagramTest {
     }
 
     @Test
-    public void isAnagramTrue() {
+    public void isAnagramTestTrue() {
         Assert.assertEquals(true, anagram.isAnagram("listen", "silent"));
+    }
+
+    @Test
+    public void groupAnagramsTestOne() {
+        List<List<String>> list = anagram.groupAnagrams(new String[]{"listen", "silent"});
+        assert(list.size()==1);
+        Assert.assertEquals(true, list.stream().anyMatch(element -> element.contains("listen")));
+        Assert.assertEquals(true, list.stream().anyMatch(element -> element.contains("silent")));
+    }
+
+    @Test
+    public void groupAnagramsTestTwo() {
+        List<List<String>> list = anagram.groupAnagrams(new String[]{"listen", "tennis", "silent"});
+        assert(list.size()==2);
+        Assert.assertEquals(true, list.stream().anyMatch(element -> element.contains("listen")));
+        Assert.assertEquals(true, list.stream().anyMatch(element -> element.contains("tennis")));
+        Assert.assertEquals(true, list.stream().anyMatch(element -> element.contains("silent")));
+    }
+
+    @Test
+    public void groupAnagramsTestThree() {
+        List<List<String>> list = anagram.groupAnagrams(new String[]{"listen", "tennis", "ten"});
+        assert(list.size()==3);
+        Assert.assertEquals(true, list.stream().anyMatch(element -> element.contains("listen")));
+        Assert.assertEquals(true, list.stream().anyMatch(element -> element.contains("tennis")));
+        Assert.assertEquals(true, list.stream().anyMatch(element -> element.contains("ten")));
+        Assert.assertEquals(false, list.stream().anyMatch(element -> element.contains("te")));
     }
 
 }
