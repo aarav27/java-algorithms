cae8c646d1398f98126552199a212baf8a791d7d
aaravsharma <aaravsharma927@gmail.com>
2020-10-04 18:13:37 -0700

    A left rotation operation on an array of size shifts each of the array's elements unit to the left and its Junit Tests.

---
+++ src/main/java/org/sharma/aarav/java/algorithms/arrays/Rotate.java
@@ -0,0 +1,28 @@
+package org.sharma.aarav.java.algorithms.arrays;
+
+//3. Rotate an array of size shifts each of the array's elements
+
+public class Rotate {
+
+    //3. A left rotation operation on an array of size shifts each of the array's elements unit to the left.
+    //  For example, if 4 left rotations are performed on array [1,2,3,4,5] then array would become [5,4,3,2,1].
+
+    public int[] getLeftRotatedArray(int[] arr, int rotate) {
+        if(arr==null || arr.length < 2 || rotate==0) {
+            return arr;
+        }
+
+        int length = arr.length;
+        int[] result = new int[length];
+
+        // Required, if rotate is greater than length
+        rotate = rotate % length;
+
+        for(int i = 0; i<length; i++) {
+            int newIndex = (i + (length - rotate)) % length;
+            result[newIndex] = arr[i];
+        }
+        return result;
+    }
+
+}
