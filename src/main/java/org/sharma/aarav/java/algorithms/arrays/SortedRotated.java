7aa1d86f87a2fb7d2af660dd57fbcdd24e8ac149
aaravsharma <aaravsharma927@gmail.com>
2020-10-14 09:02:43 -0700

    Search target integer value in a sorted rotated array, return index and its JUnit Tests.

---
+++ src/main/java/org/sharma/aarav/java/algorithms/arrays/SortedRotated.java
@@ -0,0 +1,37 @@
+package org.sharma.aarav.java.algorithms.arrays;
+
+//8. Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+// (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+// Given a target value to search. If found in the array return its index, otherwise return -1.
+// Assume no duplicate exists in the array.
+
+public class SortedRotated {
+
+    public Integer search(int[] nums, int target) {
+        if(nums == null) return null;
+
+        int minIdx = findMinIdx(nums);
+        if (target == nums[minIdx]) return minIdx;
+        int len = nums.length;
+        int start = (target <= nums[len - 1]) ? minIdx : 0;
+        int end = (target > nums[len - 1]) ? minIdx : len - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) return mid;
+            else if (target > nums[mid]) start = mid + 1;
+            else end = mid - 1;
+        }
+        return -1;
+    }
+
+    private int findMinIdx(int[] nums) {
+        int start = 0, end = nums.length - 1;
+        while (start < end) {
+            int mid = start + (end -  start) / 2;
+            if (nums[mid] > nums[end]) start = mid + 1;
+            else end = mid;
+        }
+        return start;
+    }
+}
