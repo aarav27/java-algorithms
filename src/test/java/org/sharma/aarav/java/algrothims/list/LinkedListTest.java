0cdf34e593e73b21275fe0b0c28e86055695cdf4
aaravsharma <aaravsharma927@gmail.com>
2020-10-28 22:05:49 -0700

    Append Node to Tail of a LinkedList. Given Head node and a node to append as input ;and its Junit Tests.

--- src/test/java/org/sharma/aarav/java/algrothims/list/LinkedListTest.java
+++ src/test/java/org/sharma/aarav/java/algrothims/list/LinkedListTest.java
@@ -1,6 +1,7 @@
 package org.sharma.aarav.java.algrothims.list;
 
-// 1. Junit to test to Append Node to Tail of LinkedList :: Given head node and data for append node as input
+// 1. Junit to test to Append Node to Tail of LinkedList :: Given head node and data to append as input
+// 2. Junit to test to Append Node to Tail of LinkedList :: Given head node and a node to append as input
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -46,6 +47,42 @@ public class LinkedListTest {
         Assert.assertEquals(2, head.next.data);
         Assert.assertEquals(3, head.next.next.data);
         Assert.assertEquals(4, head.next.next.next.data);
+        Assert.assertNull(head.next.next.next.next);
+    }
+
+    @Test
+    public void appendOneNodeInTailOfLinkedListTestGivenHeadNode() {
+        LinkedList.Node node = new LinkedList.Node(11);
+        linkedList.append(head, node);
+        Assert.assertEquals(1, head.data);
+        Assert.assertEquals(11, head.next.data);
+        Assert.assertNull(head.next.next);
+    }
+
+    @Test
+    public void appendTwoNodeInTailOfLinkedListTestGivenHeadNode() {
+        LinkedList.Node node1 = new LinkedList.Node(11);
+        LinkedList.Node node2 = new LinkedList.Node(21);
+        linkedList.append(head, node1);
+        linkedList.append(head, node2);
+        Assert.assertEquals(1, head.data);
+        Assert.assertEquals(11, head.next.data);
+        Assert.assertEquals(21, head.next.next.data);
+        Assert.assertNull(head.next.next.next);
+    }
+
+    @Test
+    public void appendThreeNodeInTailOfLinkedListTestGivenHeadNode() {
+        LinkedList.Node node1 = new LinkedList.Node(11);
+        LinkedList.Node node2 = new LinkedList.Node(21);
+        LinkedList.Node node3 = new LinkedList.Node(31);
+        linkedList.append(head, node1);
+        linkedList.append(head, node2);
+        linkedList.append(head, node3);
+        Assert.assertEquals(1, head.data);
+        Assert.assertEquals(11, head.next.data);
+        Assert.assertEquals(21, head.next.next.data);
+        Assert.assertEquals(31, head.next.next.next.data);
         Assert.assertNull(head.next.next.next.next);
     }
 
