771464e661ab57eb443f22e3d6c69d0e737c7e6e
aaravsharma <aaravsharma927@gmail.com>
2021-01-03 20:24:10 -0800

    Implement a Stack (having push, pop, peek and isEmpty functions) and its Junit Tests.

---
+++ src/main/java/org/sharma/aarav/java/algorithms/filo/Stack.java
@@ -0,0 +1,32 @@
+package org.sharma.aarav.java.algorithms.filo;
+
+// 1. Implement a Stack (having push, pop, peek and isEmpty functions).
+
+public class Stack<T> {
+
+    public Node<T> top;
+
+    public void push(T item) {
+        if(item == null) return;
+        Node<T> node = new Node<T>(item);
+        node.next = top;
+        top = node;
+    }
+
+    public T pop() {
+        if(top==null) return null;
+        T item = top.data;
+        top = top.next;
+        return item;
+    }
+
+    public T peek() {
+        if(top == null) return null;
+        return top.data;
+    }
+
+    public boolean isEmpty() {
+        return top == null;
+    }
+
+}
