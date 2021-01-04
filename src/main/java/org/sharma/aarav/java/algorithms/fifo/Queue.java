7b63b422fd9c79d4a86a63d8c864e7e7a586c7c7
aaravsharma <aaravsharma927@gmail.com>
2021-01-04 11:04:06 -0800

    Implement a Queue (having enqueue, dequeue, peek and isEmpty functions) and its Junit Tests.

---
+++ src/main/java/org/sharma/aarav/java/algorithms/fifo/Queue.java
@@ -0,0 +1,43 @@
+package org.sharma.aarav.java.algorithms.fifo;
+
+// 1. Implement a Queue (having enqueue, dequeue, peek and isEmpty functions).
+
+public class Queue<T> {
+
+    public Node<T> first;
+    public Node<T> last;
+
+    public void enqueue(T item) {
+        if(item == null) return;
+        Node<T> node = new Node<T>(item);
+        if(last != null && first == last) {
+            first.next = node;
+        } else if(first != last)  {
+            last.next = node;
+        }
+        last = node;
+        if(first == null) {
+            first = last;
+        }
+    }
+
+    public T dequeue() {
+        if(first == null) return null;
+        T item = first.data;
+        first = first.next;
+        if(last == null) {
+            first = null;
+        }
+        return item;
+    }
+
+    public T peek() {
+        if(first == null) return null;
+        return first.data;
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+}
